93203bee0cfd99fd27f774c825fccdfeff689dfb
Christian Lussier <lussierc@allegheny.edu>
2021-03-25 19:08:05 -0400

    Implement code that ranks search results by tag weight first, then operation weight
    Also improves commenting of the code and some basic formatting issues

--- swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/opsFilter.js
+++ swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/opsFilter.js
@@ -1,56 +1,76 @@
 export default function(taggedOps, phrase) {
-  // return taggedOps.filter((tagObj, tag) => tag.indexOf(phrase) !== -1)
-
-  // create regular expression using the phrase
-  // the modifiers ig mean that the regex will be
-  // case insensitive and match all occurrences
-  let re = new RegExp(phrase, "ig");
-  // iterate through key and value pairs within the object
+  let re = new RegExp(phrase, "ig"); // create a reg expression using the phrase (case insentitive & matches all occurences)
+  // iterate through key and value pairs within the taggedOps object:
   for (let [key, value] of taggedOps) {
-    // Count matches in every operation and
-    // sort the list of operations
-    let filteredOps = value.get("operations");
-    if (filteredOps.size !== 0) {
-      for (let i = 0; i < filteredOps.size; i++) {
-        let op = filteredOps.get(i);
-        let weight = 0;
-        // list of matches in path key
-        let pathMatches = op.get("path").match(re);
-        // list of matches in ["operation", "description"] key
-        let descMatches = op.getIn(["operation", "description"]).match(re);
 
-        // Weight of path match = 100
-        if (pathMatches) {
-          weight += pathMatches.length * 100;
-        }
-        // Weight of description match = 1
-        if (descMatches) {
-          weight += descMatches.length;
-        }
-        if (weight === 0) {
-          // remove the operation with zero matches
-          filteredOps = filteredOps.delete(i);
-        } else {
-          // add the weight key to the operation
+    let tagWeight = 0; // used to track weight of the tag (big category)
+    let keyMatches = key.toString().match(re); // find number of matches in the tag
 
-          op = op.set("weight", weight);
-          filteredOps = filteredOps.set(i, op);
-        }
-        console.log(JSON.stringify(filteredOps));
-      }
+    if (keyMatches) {
+      tagWeight += 1000;
     }
 
+    let filteredOps = value.get("operations"); // get the operations from the filteredOps
+
+    if (filteredOps.size !== 0) {
+      for (let i = 0; i < filteredOps.size; i++) {
+        let op = filteredOps.get(i); // get the current operation from filteredOps
+        let opWeight = 0; // set the base value of variable that will hold the ranking score of a operation
+
+        let pathMatches = op.get("path").match(re); // list of matches in path key (eight )
+        let descMatches = op.getIn(["operation", "description"]).match(re); // list of matches in ["operation", "description"] key
+
+        // update opWeight per the matches found:
+        if (pathMatches) {
+          opWeight += pathMatches.length * 100; // opWeight of path match = 100
+        }
+        if (descMatches) {
+          opWeight += descMatches.length; // opWeight of description match = 1
+        }
+
+        // see if the current operation contained any matches:
+        if (opWeight === 0) {
+          filteredOps = filteredOps.delete(i); // remove the operation with zero matches
+        } else {
+          filteredOps = filteredOps.set(i, op.set("opWeight", opWeight)); // add the opWeight key to the operation info
+          tagWeight += opWeight; // update the overall tag weight to reflect the operation within it's weight
+        }
+      }
+
+      // sort the tag's operations by weight:
+      filteredOps = filteredOps.sort(function(value1, value2) {
+        if (value1.get("opWeight") > value2.get("opWeight")) {
+          return -1;
+        }
+        if (value1.get("opWeight") < value2.get("opWeight")) {
+          return 1;
+        }
+        if (value1.get("opWeight") === value2.get("opWeight")) {
+          return 0;
+        }
+      });
+    }
     // Set filtered operations to respective tag
     taggedOps = taggedOps.setIn([key.toString(), "operations"], filteredOps);
 
-    if (
-      taggedOps.getIn([key.toString(), "operations"]).isEmpty() &&
-      taggedOps
-        .getIn([key.toString(), "tagDetails", "description"])
-        .indexOf(phrase) === -1
-    ) {
-      taggedOps = taggedOps.delete(key.toString());
+    // see if the current tag contained any matches:
+    if (tagWeight === 0) {
+      taggedOps = taggedOps.delete(key.toString());  // delete tags with no matches
+    } else {
+      taggedOps = taggedOps.setIn([key.toString(), "tagWeight"], tagWeight);
     }
   }
-  return taggedOps;
+  // sort the tags by weight:
+  taggedOps = taggedOps.sort(function(value1, value2) {
+    if (value1.get("tagWeight") > value2.get("tagWeight")) {
+      return -1;
+    }
+    if (value1.get("tagWeight") < value2.get("tagWeight")) {
+      return 1;
+    }
+    if (value1.get("tagWeight") === value2.get("tagWeight")) {
+      return 0;
+    }
+  })
+  return taggedOps; // return the sorted tags and their operations
 }
