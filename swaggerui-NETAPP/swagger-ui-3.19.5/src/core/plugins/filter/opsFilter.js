8713b1db11ad44b647e8f898de71ddf7ebbb892e
noorbuchi <buchin@allegheny.edu>
2021-03-25 15:25:56 -0400

    add weight and regex matching

--- swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/opsFilter.js
+++ swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/opsFilter.js
@@ -1,17 +1,56 @@
-export default function(taggedOps, phrase) { 
+export default function(taggedOps, phrase) {
   // return taggedOps.filter((tagObj, tag) => tag.indexOf(phrase) !== -1)
-  for(let [key, value] of taggedOps) {
-    // getIn works similar to taggedOps.tag.operation.
-    // Filter operations by path names
-    const filteredOps = taggedOps.getIn([key.toString(),"operations"]).filter(o => o.get('path').indexOf(phrase) !== -1 || 
-    o.getIn(['operation', 'description']).indexOf(phrase) !== -1)
-    // Set filtered operations to respective tag
-    taggedOps = taggedOps.setIn([key.toString(),"operations"], filteredOps);
 
-    if(taggedOps.getIn([key.toString(),"operations"]).isEmpty() && taggedOps.getIn([key.toString(),'tagDetails', 'description']).indexOf(phrase) === -1)
-    {
-      taggedOps = taggedOps.delete(key.toString())
+  // create regular expression using the phrase
+  // the modifiers ig mean that the regex will be
+  // case insensitive and match all occurrences
+  let re = new RegExp(phrase, "ig");
+  // iterate through key and value pairs within the object
+  for (let [key, value] of taggedOps) {
+    // Count matches in every operation and
+    // sort the list of operations
+    let filteredOps = value.get("operations");
+    if (filteredOps.size !== 0) {
+      for (let i = 0; i < filteredOps.size; i++) {
+        let op = filteredOps.get(i);
+        let weight = 0;
+        // list of matches in path key
+        let pathMatches = op.get("path").match(re);
+        // list of matches in ["operation", "description"] key
+        let descMatches = op.getIn(["operation", "description"]).match(re);
+
+        // Weight of path match = 100
+        if (pathMatches) {
+          weight += pathMatches.length * 100;
+        }
+        // Weight of description match = 1
+        if (descMatches) {
+          weight += descMatches.length;
+        }
+        if (weight === 0) {
+          // remove the operation with zero matches
+          filteredOps = filteredOps.delete(i);
+        } else {
+          // add the weight key to the operation
+
+          op = op.set("weight", weight);
+          filteredOps = filteredOps.set(i, op);
+        }
+        console.log(JSON.stringify(filteredOps));
+      }
     }
-  }  
-    return taggedOps
+
+    // Set filtered operations to respective tag
+    taggedOps = taggedOps.setIn([key.toString(), "operations"], filteredOps);
+
+    if (
+      taggedOps.getIn([key.toString(), "operations"]).isEmpty() &&
+      taggedOps
+        .getIn([key.toString(), "tagDetails", "description"])
+        .indexOf(phrase) === -1
+    ) {
+      taggedOps = taggedOps.delete(key.toString());
+    }
+  }
+  return taggedOps;
 }
