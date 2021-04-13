201d3811c7c2ae6a0c3d88fef14a7f15a7ee42a2
bagashvilit <bagashvilit@allegheny.edu>
2021-04-13 19:11:09 +0400

    feat: filter the first level properties in the model

--- swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/modelsFilter.js
+++ swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/modelsFilter.js
@@ -1,3 +1,30 @@
 export default function(models, phrase) {
-    return models.filter((model, name) => name.indexOf(phrase) !== -1)
+
+    let re = new RegExp(phrase, "ig");
+    for (let [name,map] of models){
+        
+        if(!name.toString().match(re)){
+
+            let properties = map.get("properties")
+            // Check that the propert is not undefined
+            if(properties){
+                let matches = []
+                // Iterate through all the properties for the model
+                for (let [k, v] of properties){
+                    if (k.toString().match(re)){
+                        matches.push(k)
+                    }
+                }
+                // If no match is found in the properties than delete the model
+                if(matches.length === 0){
+                    models = models.delete(name.toString());                   
+                }            
+            } else{
+                // Delete the model with undefined property
+                models = models.delete(name.toString());
+            }
+        }
+
+    }
+    return models
 }
