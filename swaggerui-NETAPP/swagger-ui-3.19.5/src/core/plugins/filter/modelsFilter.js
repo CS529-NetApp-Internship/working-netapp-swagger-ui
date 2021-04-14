6ade1c0b1b5631afc1292041d2f36a8a3751066f
colemank <colemank@allegheny.edu>
2021-04-13 22:50:55 -0400

    Sort the models

--- swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/modelsFilter.js
+++ swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/modelsFilter.js
@@ -1,30 +1,50 @@
 export default function(models, phrase) {
 
-    let re = new RegExp(phrase, "ig");
+    let re = new RegExp(phrase, "ig")
     for (let [name,map] of models){
-        
-        if(!name.toString().match(re)){
-
+        let modelWeight = 0
+        if(name.toString().match(re)) {
+            modelWeight = 10
+        }
+        else {
             let properties = map.get("properties")
-            // Check that the propert is not undefined
+            // Check that the property is not undefined
             if(properties){
-                let matches = []
                 // Iterate through all the properties for the model
                 for (let [k, v] of properties){
-                    if (k.toString().match(re) || (v.has("description") && v.get("description").match(re))){
-                        matches.push(k)
+                    if (k.toString().match(re) || (v.get("description") && v.get("description").match(re))){
+                        if (k.toString().match(re)) {
+                            modelWeight += 5
+                        }
+                        else {
+                            modelWeight++
+                        }
                     }
-                }
-                // If no match is found in the properties than delete the model
-                if(matches.length === 0){
-                    models = models.delete(name.toString());                   
-                }            
+                }         
             } else{
                 // Delete the model with undefined property
-                models = models.delete(name.toString());
+                models = models.delete(name.toString())
             }
+        }
+        if (modelWeight === 0) {
+            models = models.delete(name.toString())
+        }
+        else {
+            map = map.set("modelWeight", modelWeight)
+            models = models.set(name.toString(), map)
         }
 
     }
-    return models
+
+    return models.sort(function(value1, value2) {
+        if (value1.get("modelWeight") > value2.get("modelWeight")) {
+          return -1
+        }
+        if (value1.get("modelWeight") < value2.get("modelWeight")) {
+          return 1
+        }
+        if (value1.get("modelWeight") === value2.get("modelWeight")) {
+          return 0
+        }
+      })
 }
