df2f5550285f8ad12a5aa9d646d52128006c0dbc
noorbuchi <buchin@allegheny.edu>
2021-05-12 01:54:56 -0400

    clean up conditional logic and add call to new function

--- swaggerui-NETAPP/swagger-ui-3.19.5/src/core/components/operations.jsx
+++ swaggerui-NETAPP/swagger-ui-3.19.5/src/core/components/operations.jsx
@@ -1,16 +1,20 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Im from "immutable"
+import React from "react";
+import PropTypes from "prop-types";
+import Im from "immutable";
 
 const SWAGGER2_OPERATION_METHODS = [
-  "get", "put", "post", "delete", "options", "head", "patch"
-]
+  "get",
+  "put",
+  "post",
+  "delete",
+  "options",
+  "head",
+  "patch"
+];
 
-const OAS3_OPERATION_METHODS = SWAGGER2_OPERATION_METHODS.concat(["trace"])
-
+const OAS3_OPERATION_METHODS = SWAGGER2_OPERATION_METHODS.concat(["trace"]);
 
 export default class Operations extends React.Component {
-
   static propTypes = {
     specSelectors: PropTypes.object.isRequired,
     specActions: PropTypes.object.isRequired,
@@ -32,124 +36,123 @@ export default class Operations extends React.Component {
       layoutActions,
       getConfigs,
       fn
-    } = this.props
+    } = this.props;
 
-    let taggedOps = specSelectors.taggedOperations()
-    let definitions = specSelectors.definitions()
+    let taggedOps = specSelectors.taggedOperations();
+    let definitions = specSelectors.definitions();
+    const OperationContainer = getComponent("OperationContainer", true);
+    const OperationTag = getComponent("OperationTag");
+    const LongDescription = getComponent("LongDescription");
 
-    const OperationContainer = getComponent("OperationContainer", true)
-    const OperationTag = getComponent("OperationTag")
-    const LongDescription = getComponent("LongDescription")
+    let { maxDisplayedTags } = getConfigs();
 
-    let {
-      maxDisplayedTags,
-    } = getConfigs()
-
-    let filter = layoutSelectors.currentFilter()
+    let filter = layoutSelectors.currentFilter();
     // console.log("FINALFILTER:", filter)
-    let options = layoutSelectors.currentOptions()
+    let options = layoutSelectors.currentOptions();
     // console.log("FINALOPTIONS:", options)
-    let radioValue = layoutSelectors.radioValue()
+    let radioValue = layoutSelectors.radioValue();
 
-  if(options){
-      if (filter) {
-        if (filter !== true) {
-          if (options['endpoints'] === true) {
-                taggedOps = fn.opsFilter(taggedOps, filter, options, definitions, radioValue);
+    if (radioValue && filter && filter !== true) {
+      if (radioValue === "operations") {
+        if (options) {
+          if (options["endpoints"] === true) {
+            taggedOps = fn.opsFilter(taggedOps, filter, options);
           }
+        }
+      } else if (radioValue === "models") {
+        taggedOps = fn.endpointModelsFilter(taggedOps, definitions, filter);
+      } else {
+        console.log("Unknown radio value");
       }
     }
-  }
-  if(radioValue) {
-    if (filter) {
-      if (filter !== true) {
-      if (radioValue === "models") {
-        definitions = fn.modelsFilter(definitions, filter);
-        taggedOps = fn.opsFilter(taggedOps, filter, options, definitions, radioValue);
-      }
-    }
-    }
-  }
 
     if (maxDisplayedTags && !isNaN(maxDisplayedTags) && maxDisplayedTags >= 0) {
-      taggedOps = taggedOps.slice(0, maxDisplayedTags)
+      taggedOps = taggedOps.slice(0, maxDisplayedTags);
     }
 
     return (
-        <div>
-          {
-            taggedOps.map( (tagObj, tag) => {
-              const operations = tagObj.get("operations")
-              let tagLongDescription = tagObj.getIn(["tagDetails", "x-ntap-long-description"], null)
-              return (
-                <OperationTag
-                  key={"operation-" + tag}
-                  tagObj={tagObj}
-                  tag={tag}
-                  layoutSelectors={layoutSelectors}
-                  layoutActions={layoutActions}
-                  getConfigs={getConfigs}
-                  getComponent={getComponent}>
-                  {
-                    operations.map( op => {
-                      const path = op.get("path")
-                      let method = op.get("method")
-                      const specPath = Im.List(["paths", path, method])
+      <div>
+        {taggedOps
+          .map((tagObj, tag) => {
+            const operations = tagObj.get("operations");
+            let tagLongDescription = tagObj.getIn(
+              ["tagDetails", "x-ntap-long-description"],
+              null
+            );
+            return (
+              <OperationTag
+                key={"operation-" + tag}
+                tagObj={tagObj}
+                tag={tag}
+                layoutSelectors={layoutSelectors}
+                layoutActions={layoutActions}
+                getConfigs={getConfigs}
+                getComponent={getComponent}
+              >
+                {operations
+                  .map(op => {
+                    const path = op.get("path");
+                    let method = op.get("method");
+                    const specPath = Im.List(["paths", path, method]);
 
-                      let introduced = undefined
-                      if (method != 'x-ntap-long-description') {
-			    introduced = op.getIn(["operation", "x-ntap-introduced"])
+                    let introduced = undefined;
+                    if (method != "x-ntap-long-description") {
+                      introduced = op.getIn(["operation", "x-ntap-introduced"]);
+                    }
+
+                    // FIXME: (someday) this logic should probably be in a selector,
+                    // but doing so would require further opening up
+                    // selectors to the plugin system, to allow for dynamic
+                    // overriding of low-level selectors that other selectors
+                    // rely on. --KS, 12/17
+                    const validMethods = specSelectors.isOAS3()
+                      ? OAS3_OPERATION_METHODS
+                      : SWAGGER2_OPERATION_METHODS;
+
+                    if (validMethods.indexOf(method) === -1) {
+                      if (method === "x-ntap-long-description") {
+                        var cleanPath = path.replace(/\//g, "_");
+                        if (cleanPath[0] === "_") {
+                          cleanPath = cleanPath.substr(1);
+                        }
+                        return (
+                          <LongDescription
+                            key={["docs", tag, cleanPath]}
+                            description={op.getIn(["operation", "description"])}
+                            title={path}
+                            isShownKey={["docs", tag, cleanPath]}
+                            layoutActions={layoutActions}
+                            layoutSelectors={layoutSelectors}
+                            getComponent={getComponent}
+                            getConfigs={getConfigs}
+                          />
+                        );
                       }
+                      return null;
+                    }
 
-                      // FIXME: (someday) this logic should probably be in a selector,
-                      // but doing so would require further opening up
-                      // selectors to the plugin system, to allow for dynamic
-                      // overriding of low-level selectors that other selectors
-                      // rely on. --KS, 12/17
-                      const validMethods = specSelectors.isOAS3() ?
-                            OAS3_OPERATION_METHODS : SWAGGER2_OPERATION_METHODS
+                    return (
+                      <OperationContainer
+                        key={`${path}-${method}`}
+                        specPath={specPath}
+                        op={op}
+                        path={path}
+                        method={method}
+                        tag={tag}
+                        introduced={introduced}
+                      />
+                    );
+                  })
+                  .toArray()}
+              </OperationTag>
+            );
+          })
+          .toArray()}
 
-                      if(validMethods.indexOf(method) === -1) {
-                        if (method === 'x-ntap-long-description') {
-                            var cleanPath = path.replace(/\//g, '_');
-                            if (cleanPath[0] === '_') { cleanPath = cleanPath.substr(1); }
-                            return <LongDescription
-                              key={ [ "docs", tag, cleanPath ] }
-                              description={ op.getIn(["operation","description"]) }
-                              title={ path }
-                              isShownKey={ [ "docs", tag, cleanPath ] }
-                              layoutActions={ layoutActions }
-                              layoutSelectors={ layoutSelectors }
-                              getComponent={ getComponent }
-                              getConfigs={ getConfigs }
-                            />
-                          }
-                        return null
-                      }
-
-                      return <OperationContainer
-                                 key={`${path}-${method}`}
-                                 specPath={specPath}
-                                 op={op}
-                                 path={path}
-                                 method={method}
-                                 tag={tag}
-                                 introduced={introduced}
-                                 />
-                    }).toArray()
-                  }
-
-
-                </OperationTag>
-              )
-            }).toArray()
-          }
-
-          { taggedOps.size < 1 ? <h3> No operations defined in spec! </h3> : null }
-        </div>
-    )
+        {taggedOps.size < 1 ? <h3> No operations defined in spec! </h3> : null}
+      </div>
+    );
   }
-
 }
 
 Operations.propTypes = {
@@ -159,4 +162,4 @@ Operations.propTypes = {
   layoutSelectors: PropTypes.object.isRequired,
   getComponent: PropTypes.func.isRequired,
   fn: PropTypes.object.isRequired
-}
+};
--- swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/opsFilter.js
+++ swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/opsFilter.js
@@ -1,9 +1,9 @@
-export default function(taggedOps, phrase, options, definitions, radioValue) {
+export default function(taggedOps, phrase, options) {
   // return taggedOps.filter((tagObj, tag) => tag.indexOf(phrase) !== -1)
   // create regular expression using the phrase
   // the modifiers ig mean that the regex will be
   // case insensitive and match all occurrences
-
+  console.log("Im being called");
   let re = new RegExp(phrase, "ig");
   // iterate through key and value pairs within the object
   for (let [key, value] of taggedOps) {
@@ -20,28 +20,24 @@ export default function(taggedOps, phrase, options, definitions, radioValue) {
       for (let i = 0; i < filteredOps.size; i++) {
         let op = filteredOps.get(i);
         let opWeight = 0;
-        if (radioValue === "models") {
-          if (op.get("operation").has("responses")) {
-            for (let [key2, value2] of op.getIn(["operation", "responses"])) {
-                if (definitions.has(value2.getIn(["schema", "$ref"], " ").split("definitions/")[1])) {
-                  opWeight += 50;
-                }
-              }
-            }
-        }
-        else {
-
         // check if the path checkbox is checked and if none of the sub-checkboxes are checked
         // If this is true, then count the number of matches in the path name
-          if (options["endpointsOptions"]["paths"] || ((options["endpoints"]) && 
-          !(options["endpointsOptions"]["paths"] || options["endpointsOptions"]["description"] || options["endpointsOptions"]["method"]))) {
-             // opWeight of path match = 100
+        if (
+          options["endpointsOptions"]["paths"] ||
+          (options["endpoints"] &&
+            !(
+              options["endpointsOptions"]["paths"] ||
+              options["endpointsOptions"]["description"] ||
+              options["endpointsOptions"]["method"]
+            ))
+        ) {
+          // opWeight of path match = 100
           let pathMatches = op.get("path").match(re);
           if (pathMatches) {
             opWeight += pathMatches.length * 100;
           }
         }
-          if (options["endpointsOptions"]["description"]) {
+        if (options["endpointsOptions"]["description"]) {
           // list of matches in ["operation", "description"] key
           let descMatches = op.getIn(["operation", "description"]).match(re);
 
@@ -50,30 +46,35 @@ export default function(taggedOps, phrase, options, definitions, radioValue) {
             opWeight += descMatches.length;
           }
         }
+
+        // If there are no matches or if the method box is checked, but doesn't have the method defined in the search, then remove the operation
+        if (
+          opWeight === 0 ||
+          (options &&
+            options["endpointsOptions"]["method"] &&
+            !options["endpointsOptions"]["methodOptions"][op.get("method")])
+        ) {
+          filteredOps = filteredOps.delete(filteredOps.indexOf(op));
+          i -= 1;
+        } else {
+          // add the opWeight key to the operation
+          filteredOps = filteredOps.set(i, op.set("opWeight", opWeight));
+          tagWeight += opWeight;
+        }
       }
-          //If there are no matches or if the method box is checked, but doesn't have the method defined in the search, then remove the operation
-          if (opWeight === 0 || (options && options["endpointsOptions"]["method"] && !options["endpointsOptions"]["methodOptions"][op.get("method")])) {
-              filteredOps = filteredOps.delete(filteredOps.indexOf(op));
-              i-=1;
-            } else {
-              // add the opWeight key to the operation
-              filteredOps = filteredOps.set(i, op.set("opWeight", opWeight));
-              tagWeight += opWeight;
-              }
-          }
-        }
-      filteredOps = filteredOps.sort(function(value1, value2) {
-        if (value1.get("opWeight") > value2.get("opWeight")) {
-          return -1;
-        }
-        if (value1.get("opWeight") < value2.get("opWeight")) {
-          return 1;
-        }
-        if (value1.get("opWeight") === value2.get("opWeight")) {
-          return 0;
-        }
-      });
-    
+    }
+    filteredOps = filteredOps.sort(function(value1, value2) {
+      if (value1.get("opWeight") > value2.get("opWeight")) {
+        return -1;
+      }
+      if (value1.get("opWeight") < value2.get("opWeight")) {
+        return 1;
+      }
+      if (value1.get("opWeight") === value2.get("opWeight")) {
+        return 0;
+      }
+    });
+
     // Set filtered operations to respective tag
     taggedOps = taggedOps.setIn([key.toString(), "operations"], filteredOps);
 
@@ -84,16 +85,16 @@ export default function(taggedOps, phrase, options, definitions, radioValue) {
       taggedOps = taggedOps.setIn([key.toString(), "tagWeight"], tagWeight);
     }
   }
-    taggedOps = taggedOps.sort(function(value1, value2) {
-      if (value1.get("tagWeight") > value2.get("tagWeight")) {
-        return -1;
-      }
-      if (value1.get("tagWeight") < value2.get("tagWeight")) {
-        return 1;
-      }
-      if (value1.get("tagWeight") === value2.get("tagWeight")) {
-        return 0;
-      }
-    });
-    return taggedOps;
-  }
+  taggedOps = taggedOps.sort(function(value1, value2) {
+    if (value1.get("tagWeight") > value2.get("tagWeight")) {
+      return -1;
+    }
+    if (value1.get("tagWeight") < value2.get("tagWeight")) {
+      return 1;
+    }
+    if (value1.get("tagWeight") === value2.get("tagWeight")) {
+      return 0;
+    }
+  });
+  return taggedOps;
+}
