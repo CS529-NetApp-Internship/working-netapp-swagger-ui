8e2d1224004e188146bf76bd16bc1953748e4e63
noorbuchi <buchin@allegheny.edu>
2021-05-12 20:55:38 -0400

    use new filtering approach

--- swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/endpointModelsFilter.js
+++ swaggerui-NETAPP/swagger-ui-3.19.5/src/core/plugins/filter/endpointModelsFilter.js
@@ -1,16 +1,16 @@
 import modelsFilter from "./modelsFilter";
+import { Map, List, fromJS } from "immutable";
 
 export default function(taggedOps, modelsDefinition, phrase) {
   // duplicative work is happening here is with the calls to modelsFilter
   const filteredModels = modelsFilter(modelsDefinition, phrase);
-
   // iterate through taggedOps
   for (let [key, value] of taggedOps) {
     // filteredOps is a list of Objects
+    let foundMatches = [];
     let filteredOps = value.get("operations");
     for (let i = 0; i < filteredOps.size; i++) {
       let op = filteredOps.get(i);
-      let foundMatch = false;
       if (op.get("method") != "x-ntap-long-description") {
         // responses is nested object
         let responses = op.getIn(["operation", "responses"]);
@@ -24,23 +24,22 @@ export default function(taggedOps, modelsDefinition, phrase) {
               let foundModelName = ref.substring(firstModelStringIndex);
               //  check if the found model name exists in the filtered models
               if (filteredModels.get(foundModelName)) {
-                foundMatch = true;
+                foundMatches.push(op);
+                break;
               }
             }
           }
         }
       }
-      //   remove operations with no matches
-      if (foundMatch === false) {
-        filteredOps = filteredOps.delete(filteredOps.indexOf(op));
-        i -= 1;
-      }
     }
-    if (filteredOps.size === 0) {
+    if (foundMatches.length === 0) {
       taggedOps = taggedOps.delete(key.toString());
     } else {
       // Update taggedOps with the operations containing matches
-      taggedOps = taggedOps.setIn([key.toString(), "operations"], filteredOps);
+      taggedOps = taggedOps.setIn(
+        [key.toString(), "operations"],
+        List(foundMatches)
+      );
     }
   }
   return taggedOps;
